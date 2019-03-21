6096c6b7b8dcdb415670e38fd594acc62efb9d08
Michael Steil <mist64@mac.com>
2019-03-21 07:51:48 +0100

    ...

---
+++ images/convert.sh
@@ -0,0 +1,2 @@
+convert -bordercolor black -border 1 -loop 0 -delay 200 /tmp/IEEE-488/*.png ../docs/cbmbus/ieee-488.gif
+#convert -crop 420x480+0+0\! -bordercolor black -border 1  IEEE-488\ Layers.001.png  ../docs/cbmbus/ieee-488_layers.png
--- images/make_gif.sh
+++
@@ -1,2 +0,0 @@
-convert -crop 1200x710+0+0\! -bordercolor black -border 1 -loop 0 -delay 200 *.png ../docs/cbmbus/ieee-488.gif
-convert -crop 420x480+0+0\! -bordercolor black -border 1  IEEE-488\ Layers.001.png  ../docs/cbmbus/ieee-488_layers.png
