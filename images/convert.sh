084137a89f3e86d0d7e032b2b58f4a45c905449d
Michael Steil <mist64@mac.com>
2019-03-24 13:17:05 +0100

    ...

--- images/convert.sh
+++ images/convert.sh
@@ -1,2 +1,3 @@
+convert -crop 300x160+0+0\! -bordercolor black -border 1 -loop 0 -delay 100 /tmp/Open\ Collector/*.png ../docs/cbmbus/open_collector.gif
 convert -bordercolor black -border 1 -loop 0 -delay 200 /tmp/IEEE-488/*.png ../docs/cbmbus/ieee-488.gif
 #convert -crop 420x480+0+0\! -bordercolor black -border 1  IEEE-488\ Layers.001.png  ../docs/cbmbus/ieee-488_layers.png
